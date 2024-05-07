1eaf82fea92dd6080628a14c39fc805f1e4830bb
utopicUnicorn <christianbarrios995@gmail.com>
2024-05-07 11:20:52 +0800

    FM-BMI: added few functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
-import Logo from '/assets/images/logo.svg'
-import Calculator from './components/Calculator.jsx'
-import YourResult from './components/YourResult.jsx'
-import Tips from './components/Tips.jsx'
-import Limitations from './components/Limitations.jsx'
+import { useState } from "react";
+import Logo from "/assets/images/logo.svg";
+import Calculator from "./components/Calculator.jsx";
+import YourResult from "./components/YourResult.jsx";
+import Tips from "./components/Tips.jsx";
+import Limitations from "./components/Limitations.jsx";
 
 function App() {
+  const [BMI, setBMI] = useState({
+   height: 0,
+   weight: 0
+  });
+
+  console.log(BMI)
+
+
+  // function handleSetBMI(measurementType) {
+  //   switch(cm){
+  //     case 'cm':
+  //       setBMI(prevValue => ({...prevValue, weight: 50}) )
+  //   }
+  //   if(measuring === 'metric'){
+  //     setBMI(prevValue => ({...prevValue, weight: 50}) )
+  //   }else{
+
+  //   }
+    
+  // }
+
+  //metric muna
+
+  // function handleSetBMI(height, weight){
+  //   if(height){
+  //     setBMI(prevValue => ({...prevValue, height: height}) )
+  //   }else{
+  //     setBMI(prevValue => ({...prevValue, weight: weight}) )
+  //   }
+  //   console.log('function activating')
+  // }
+
+  function calculateBMI() {}
 
   return (
     <>
@@ -13,27 +47,28 @@ function App() {
       </header>
       <main>
         <section id="keyvisual">
-            <div id="keyvisualBox-bg">
-              <div className="keyvisual__text-container">
-                <h1>
-                  Body Mass<br/> Index Calculator
-                </h1>
-                <p>
-                Better understand your weight in relation to your height using our 
-                body mass index (BM) calculator. While BMI is not the sole determinant 
-                of a healthy weight, it offers a valuable starting point to evaluate 
-                your overall health and well-being.
-                </p>
-              </div>
+          <div id="keyvisualBox-bg">
+            <div className="keyvisual__text-container">
+              <h1>
+                Body Mass
+                <br /> Index Calculator
+              </h1>
+              <p>
+                Better understand your weight in relation to your height using
+                our body mass index (BM) calculator. While BMI is not the sole
+                determinant of a healthy weight, it offers a valuable starting
+                point to evaluate your overall health and well-being.
+              </p>
             </div>
-            <Calculator/>
+          </div>
+          <Calculator handleBMIValue={() => setBMI} />
         </section>
-        <YourResult/>
-        <Tips/>
-        <Limitations/>
+        <YourResult />
+        <Tips />
+        <Limitations />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,51 +1,110 @@
-import { useRef } from "react"
+import { useState } from "react";
 
+export default function Calculator({ handleBMIValue }) {
+  const [measuring, setMeasuring] = useState("metric");
 
-export default function Calculator(){
-    const radioBtn = useRef()
-
-    return <div id="calculator">
-                <h3>
-                    Enter your details below
-                </h3>
-                <div className="input-group input-radio">
-                    <div className="input-pair">
-                        <input type="radio" name="classification" id="metric" />
-                        <label htmlFor="metric">Metric</label>
-                    </div>
-                    <div className="input-pair">
-                        <input type="radio" name="classification" id="imperial" />
-                        <label htmlFor="imperial">Imperial</label>
-                    </div>
-                </div>
-                <div className="input-group input-measurements">
-                    <div className="input-pair" id="height" >
-                        <input type="number" placeholder="0" name="height" />
-                        <span>cm</span>
-                        <label htmlFor="height">Height</label>
-                    </div>
-                    <div className="input-pair"  id="weight" >
-                        <input type="number" placeholder="0" name="weight" id="weight" />
-                        <span>kg</span>
-                        <label htmlFor="weight">Weight</label>
-                    </div>
-                </div>
-                <div id="result">
-                    <div className="result-text">
-                        <p>
-                            Your BMI is...
-                        </p>
-                        <h2>
-                            23.5
-                        </h2>
-                    </div>
-
-                    <div className="result-description">
-                       <p>
-                         Your BMI suggests you’re a healthy weight. Your ideal weight is between <span>63.3kgs - 85.2kgs</span>.
-                       </p>
-                    </div>
-                </div>
+  return (
+    <div id="calculator">
+      <h3>Enter your details below</h3>
+      <div className="input-group input-radio">
+        <div className="input-pair">
+          <input
+            type="radio"
+            name="classification"
+            id="metric"
+            onChange={() => setMeasuring("metric")}
+            defaultChecked
+          />
+          <label htmlFor="metric">Metric</label>
         </div>
-        
+        <div className="input-pair">
+          <input
+            type="radio"
+            name="classification"
+            id="imperial"
+            onChange={() => setMeasuring("imperial")}
+          />
+          <label htmlFor="imperial">Imperial</label>
+        </div>
+      </div>
+      {measuring === "metric" ? (
+        <div className="input-group input-measurements">
+          <div className="input-pair" id="height">
+            <input
+              type="number"
+              placeholder="0"
+              name="height"
+              onChange={(e) =>
+                // handleBMIValue((prevValue) => ({
+                //   ...prevValue,
+                //   height: e.target.value,
+                // }))
+                console.log(handleBMIValue)
+                // console.log(e.target.value)
+              }
+            />
+            <span>cm</span>
+            <label htmlFor="height">Height</label>
+          </div>
+          <div className="input-pair" id="weight">
+            <input type="number" placeholder="0" name="weight" id="weight" />
+            <span>kg</span>
+            <label htmlFor="weight">Weight</label>
+          </div>
+        </div>
+      ) : (
+        <div className="input-group input-measurements imperial">
+          <div className="input-pair" id="height">
+            <div className="input-imperial">
+              <div className="input__imperial-container">
+                <input type="number" placeholder="0" name="height" />
+                <span>ft</span>
+              </div>
+              <div className="input__imperial-container">
+                <input type="number" placeholder="0" name="height" />
+                <span>in</span>
+              </div>
+            </div>
+            <label htmlFor="height">Height</label>
+          </div>
+          <div className="input-pair" id="weight">
+            <div className="input-imperial">
+              <div className="input__imperial-container">
+                <input
+                  type="number"
+                  placeholder="0"
+                  name="weight"
+                  id="weight"
+                />
+                <span>st</span>
+              </div>
+              <div className="input__imperial-container">
+                <input
+                  type="number"
+                  placeholder="0"
+                  name="weight"
+                  id="weight"
+                />
+                <span>lbs</span>
+              </div>
+            </div>
+            <label htmlFor="weight">Weight</label>
+          </div>
+        </div>
+      )}
+      <div id="result">
+        <div className="result-text">
+          <p>Your BMI is...</p>
+          <h2>23.5</h2>
+        </div>
+
+        <div className="result-description">
+          <p>
+            Your BMI suggests you’re a healthy weight. Your ideal weight is
+            between <span>63.3kgs - 85.2kgs</span>.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
